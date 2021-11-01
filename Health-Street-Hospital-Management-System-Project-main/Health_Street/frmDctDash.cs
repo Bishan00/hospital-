6c74d1b9bcb25285358780e7bb054b90aac71b0b
sachintha00 <sachintha0603@gmail.com>
2021-11-01 08:48:22 +0530

    updated

--- Health-Street-Hospital-Management-System-Project-main/Health_Street/frmDctDash.cs
+++ Health-Street-Hospital-Management-System-Project-main/Health_Street/frmDctDash.cs
@@ -8,17 +8,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SMDMySQLDBManager;
+using MySql.Data.MySqlClient;
 
 namespace Health_Street
 {
     public partial class frmDctDash : Form
     {
+        private SmdDbManager dbManager;
         public frmDctDash()
         {
             InitializeComponent();
-            if (SQLConnectionManager.chek("SELECT * FROM LOG_IN_USER WHERE Roll_Id = '" + frmLogin.passingRoll + "'") == 1)
+            dbManager = new SmdDbManager("SERVER=127.0.0.1; PORT=3306; DATABASE=hospital; UID=root; PASSWORD=;");
+            if (dbManager.chek("SELECT * FROM LOG_IN_USER WHERE Roll_Id = '" + frmLogin.passingRoll + "'") == 1)
             {
-                SqlDataReader reader1 = SQLConnectionManager.readAndGet("SELECT * FROM LOG_IN_USER WHERE Roll_Id = '" + frmLogin.passingRoll + "'");
+                MySqlDataReader reader1 = dbManager.ReadAndGet("SELECT * FROM LOG_IN_USER WHERE Roll_Id = '" + frmLogin.passingRoll + "'");
                 if (reader1.Read())
                 {
                     lblDCTname.Text = "Hi, Dr."+reader1[2].ToString();
@@ -30,7 +34,7 @@ namespace Health_Street
             rowCountInPatient();
 
 
-            //SqlDataReader reader3 = SQLConnectionManager.readAndGet("SELECT * FROM OUT_PATIENT");
+            //SqlDataReader reader3 = dbManager.readAndGet("SELECT * FROM OUT_PATIENT");
 
             //while(reader3.Read())
             //{
@@ -42,13 +46,13 @@ namespace Health_Street
         private void rowCountOutPatient()
         {
             DataTable dt = new DataTable();
-            dt = SQLConnectionManager.getdata("SELECT * FROM OUT_PATIENT");
+            dt = dbManager.getdata("SELECT * FROM OUT_PATIENT");
             lblOutpatient.Text = "+" + dt.Rows.Count.ToString();
         }
         private void rowCountInPatient()
         {
             DataTable dt = new DataTable();
-            dt = SQLConnectionManager.getdata("SELECT * FROM PATIENT_PRIVATE_DETAIL,GUARDIAN,IN_PATIENT WHERE PATIENT_PRIVATE_DETAIL.Guardian_Id_Number = GUARDIAN.Guardian_Id_Number AND PATIENT_PRIVATE_DETAIL.Patient_Id_Number=IN_PATIENT.Patient_Id_Number");
+            dt = dbManager.getdata("SELECT * FROM PATIENT_PRIVATE_DETAIL,GUARDIAN,IN_PATIENT WHERE PATIENT_PRIVATE_DETAIL.Guardian_Id_Number = GUARDIAN.Guardian_Id_Number AND PATIENT_PRIVATE_DETAIL.Patient_Id_Number=IN_PATIENT.Patient_Id_Number");
             lblInpatient.Text = "+" + dt.Rows.Count.ToString();
         }
 
