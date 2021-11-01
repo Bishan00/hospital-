ed6e16698648ff9a7e6785b05054d64bcc574774
sachintha00 <sachintha0603@gmail.com>
2021-11-01 09:12:04 +0530

    updated

--- Health-Street-Hospital-Management-System-Project-main/Health_Street/frmScannRoomDash.cs
+++ Health-Street-Hospital-Management-System-Project-main/Health_Street/frmScannRoomDash.cs
@@ -8,14 +8,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SMDMySQLDBManager;
+using MySql.Data.MySqlClient;
 
 namespace Health_Street
 {
     public partial class frmScannRoomDash : Form
     {
+        private SmdDbManager dbManager;
         public frmScannRoomDash()
         {
             InitializeComponent();
+            dbManager = new SmdDbManager("SERVER=127.0.0.1; PORT=3306; DATABASE=hospital; UID=root; PASSWORD=;");
             DashName();
             dataShow();
             rowCountInpatients();
@@ -25,13 +29,13 @@ namespace Health_Street
         private void rowCountInpatients()
         {
             DataTable dt = new DataTable();
-            dt = SQLConnectionManager.getdata("SELECT * FROM IN_PATIENT_SCAN");
+            dt = dbManager.getdata("SELECT * FROM IN_PATIENT_SCAN");
             lblInpatients.Text = "+" + dt.Rows.Count.ToString();
         }
         private void rowCountOutpatients()
         {
             DataTable dt = new DataTable();
-            dt = SQLConnectionManager.getdata("SELECT * FROM OUT_PATIENT_SCAN");
+            dt = dbManager.getdata("SELECT * FROM OUT_PATIENT_SCAN");
             lblOutpatients.Text = "+" + dt.Rows.Count.ToString();
         }
 
@@ -43,9 +47,9 @@ namespace Health_Street
 
         private void DashName()
         {
-            if (SQLConnectionManager.chek("SELECT * FROM LOG_IN_USER WHERE Roll_Id = '" + frmLogin.passingRoll + "'") == 1)
+            if (dbManager.chek("SELECT * FROM LOG_IN_USER WHERE Roll_Id = '" + frmLogin.passingRoll + "'") == 1)
             {
-                SqlDataReader reader1 = SQLConnectionManager.readAndGet("SELECT * FROM LOG_IN_USER WHERE Roll_Id = '" + frmLogin.passingRoll + "'");
+                MySqlDataReader reader1 = dbManager.ReadAndGet("SELECT * FROM LOG_IN_USER WHERE Roll_Id = '" + frmLogin.passingRoll + "'");
                 if (reader1.Read())
                 {
                     lblScanOfficer.Text = "Hi, " + reader1[2].ToString();
@@ -57,12 +61,12 @@ namespace Health_Street
         private void dataShow()
         {
             DataTable dt = new DataTable();
-            dt = SQLConnectionManager.getdata("SELECT * FROM SCAN_ROOM");
+            dt = dbManager.getdata("SELECT * FROM SCAN_ROOM");
             dgvStaff.AutoGenerateColumns = false;
             dgvStaff.DataSource = dt;
 
             DataTable dt2 = new DataTable();
-            dt2 = SQLConnectionManager.getdata("SELECT * FROM IN_PATIENT_SCAN,OUT_PATIENT_SCAN");
+            dt2 = dbManager.getdata("SELECT * FROM IN_PATIENT_SCAN,OUT_PATIENT_SCAN");
             dgvScann.AutoGenerateColumns = false;
             dgvScann.DataSource = dt2;
         }
