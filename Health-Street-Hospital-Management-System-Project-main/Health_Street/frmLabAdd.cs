6d118027c9f926a53698a357f0ff7ec509850016
sachintha00 <sachintha0603@gmail.com>
2021-11-01 08:53:48 +0530

    updated

--- Health-Street-Hospital-Management-System-Project-main/Health_Street/frmLabAdd.cs
+++ Health-Street-Hospital-Management-System-Project-main/Health_Street/frmLabAdd.cs
@@ -11,14 +11,18 @@ using System.Threading.Tasks;
 using System.Web.Services.Description;
 using System.Windows.Forms;
 using System.IO;
+using SMDMySQLDBManager;
+using MySql.Data.MySqlClient;
 
 namespace Health_Street
 {
     public partial class frmLabAdd : Form
     {
+        private SmdDbManager dbManager;
         public frmLabAdd(/*Form frm*/)
         {
             InitializeComponent();
+            dbManager = new SmdDbManager("SERVER=127.0.0.1; PORT=3306; DATABASE=hospital; UID=root; PASSWORD=;");
             tmrDateTime.Start();
             txtPatientId.Focus();
             cmbSample.SelectedItem = null;
@@ -77,7 +81,7 @@ namespace Health_Street
             else
             {
 
-                int i = SQLConnectionManager.insrtUpdteDelt("INSERT INTO IN_SAMPLE VALUES('" + txtPatientId.Text+ "','"+cmbSample.Text+"','" + txtWardNo.Text+"','"+txtRoomNo.Text+"','"+dct+"','"+frmLogin.passingRoll+ "',SYSDATETIME(),SYSDATETIME())");
+                int i = dbManager.insrtUpdteDelt("INSERT INTO IN_SAMPLE VALUES('" + txtPatientId.Text+ "','"+cmbSample.Text+"','" + txtWardNo.Text+"','"+txtRoomNo.Text+"','"+dct+"','"+frmLogin.passingRoll+ "',SYSDATETIME(),SYSDATETIME())");
 
                 if (i == 1)
                 {
@@ -101,15 +105,15 @@ namespace Health_Street
                 txtPatientId.BorderColor = Color.Silver;
                 txtPatientId.FocusedBorderColor = Color.FromArgb(33, 96, 104);
                 lblPatientId.ResetText();
-                if (txtPatientId.Text == SQLConnectionManager.getValue("SELECT * FROM IN_PATIENT", txtPatientId.Text, 2, 2))
+                if (txtPatientId.Text == dbManager.getValue("SELECT * FROM IN_PATIENT", txtPatientId.Text, 2, 2))
                 {
                     txtPatientId.BorderColor = Color.Silver;
                     txtPatientId.FocusedBorderColor = Color.FromArgb(33, 96, 104);
                     lblPatientId.ResetText();
-                    dct =  SQLConnectionManager.getValue("SELECT * FROM IN_PATIENT", txtPatientId.Text, 2, 8);
-                    txtWardNo.Text = SQLConnectionManager.getValue("SELECT * FROM IN_PATIENT", txtPatientId.Text, 2, 9);
-                    txtRoomNo.Text = SQLConnectionManager.getValue("SELECT * FROM IN_PATIENT", txtPatientId.Text, 2, 10);
-                    txtDctName.Text = SQLConnectionManager.getValue("SELECT * FROM SPECIALIST_DOCTOR", dct, 1, 4);
+                    dct =  dbManager.getValue("SELECT * FROM IN_PATIENT", txtPatientId.Text, 2, 8);
+                    txtWardNo.Text = dbManager.getValue("SELECT * FROM IN_PATIENT", txtPatientId.Text, 2, 9);
+                    txtRoomNo.Text = dbManager.getValue("SELECT * FROM IN_PATIENT", txtPatientId.Text, 2, 10);
+                    txtDctName.Text = dbManager.getValue("SELECT * FROM SPECIALIST_DOCTOR", dct, 1, 4);
                 }
                 else
                 {
