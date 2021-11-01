27fc43017f64f0152973b5d257ae11f5fe613438
sachintha00 <sachintha0603@gmail.com>
2021-11-01 09:05:23 +0530

    updated

--- Health-Street-Hospital-Management-System-Project-main/Health_Street/frmPhrMedAdd.cs
+++ Health-Street-Hospital-Management-System-Project-main/Health_Street/frmPhrMedAdd.cs
@@ -11,14 +11,18 @@ using System.Threading.Tasks;
 using System.Web.Services.Description;
 using System.Windows.Forms;
 using System.IO;
+using SMDMySQLDBManager;
+using MySql.Data.MySqlClient;
 
 namespace Health_Street
 {
     public partial class frmPhrMedAdd : Form
     {
+        private SmdDbManager dbManager;
         public frmPhrMedAdd(/*Form frm*/)
         {
             InitializeComponent();
+            dbManager = new SmdDbManager("SERVER=127.0.0.1; PORT=3306; DATABASE=hospital; UID=root; PASSWORD=;");
             tmrDateTime.Start();
             txtDrugName.Focus();
         }
@@ -110,7 +114,7 @@ namespace Health_Street
             else
             {
 
-                int i = SQLConnectionManager.insrtUpdteDelt("INSERT INTO DRUG VALUES('" + txtDrugName.Text + "','" + txtTradeNumber.Text + "','" + txtStorePlace.Text + "','" + txtDrugPrice.Text + "')");
+                int i = dbManager.insrtUpdteDelt("INSERT INTO DRUG VALUES('" + txtDrugName.Text + "','" + txtTradeNumber.Text + "','" + txtStorePlace.Text + "','" + txtDrugPrice.Text + "')");
 
                 if (i == 1)
                 {
