ed73dd47d30d2e25692648edc93fb4ff6b1ff508
sachintha00 <sachintha0603@gmail.com>
2021-11-01 09:18:13 +0530

    updated

--- Health-Street-Hospital-Management-System-Project-main/Health_Street/frmWdRoom.cs
+++ Health-Street-Hospital-Management-System-Project-main/Health_Street/frmWdRoom.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SMDMySQLDBManager;
+using MySql.Data.MySqlClient;
 
 namespace Health_Street
 {
     public partial class frmWdRoom : Form
     {
+        private SmdDbManager dbManager;
         public frmWdRoom()
         {
             InitializeComponent();
+            dbManager = new SmdDbManager("SERVER=127.0.0.1; PORT=3306; DATABASE=hospital; UID=root; PASSWORD=;");
             show();
         }
 
@@ -26,7 +30,7 @@ namespace Health_Street
         private void show()
         {
             DataTable dt = new DataTable();
-            dt = SQLConnectionManager.getdata("SELECT * FROM ROOM, WARD WHERE ROOM.Ward_Number = WARD.Ward_Number");
+            dt = dbManager.getdata("SELECT * FROM ROOM, WARD WHERE ROOM.Ward_Number = WARD.Ward_Number");
             dgvRooms.AutoGenerateColumns = false;
             dgvRooms.DataSource = dt;
         }
@@ -34,7 +38,7 @@ namespace Health_Street
         private void txtSearch_OnTextChange(object sender, EventArgs e)
         {
             DataTable dt = new DataTable();
-            dt = SQLConnectionManager.getdata("SELECT * FROM ROOM,WARD WHERE ROOM.Room_Number LIKE '" + txtSearch.text + "%' OR ROOM.Room_Name LIKE '" + txtSearch.text + "%' OR ROOM.Room_Type LIKE '" + txtSearch.text + "%' OR WARD.Ward_Number LIKE '" + txtSearch.text + "%'");
+            dt = dbManager.getdata("SELECT * FROM ROOM,WARD WHERE ROOM.Room_Number LIKE '" + txtSearch.text + "%' OR ROOM.Room_Name LIKE '" + txtSearch.text + "%' OR ROOM.Room_Type LIKE '" + txtSearch.text + "%' OR WARD.Ward_Number LIKE '" + txtSearch.text + "%'");
             dgvRooms.DataSource = dt;
         }
 
